82212319b5308ffafbca8263f8866beef6a18d8a
unknown <lrdwhyt@gmail.com>
2020-08-15 00:38:55 +0900

    add framework for managing player status/aliases

--- src/components/PlayerItem.vue
+++ src/components/PlayerItem.vue
@@ -1,10 +1,21 @@
 <template>
-    <div class="player-name word-edit" v-if="isEditMode">
-        <input type="text" ref="edit" :value="this.$props.name" @blur="stopEdit" @keyup="keyUp" />
-    </div>
-    <div class="player-name word-view" v-else>
-        <label @click="edit">{{ this.$props.name }}</label
-        ><button class="remove-button" @click="remove">🗙</button>
+    <div class="player-item">
+        <div class="player-name word-edit" v-if="isEditName">
+            <input type="text" ref="edit" :value="this.$props.name" @blur="stopEdit" @keyup="handleKeyup" />
+        </div>
+        <div class="player-name word-view" v-else>
+            <label @click="edit">{{ this.$props.name }}</label
+            ><button class="remove-button" @click="remove">🗙</button>
+        </div>
+        <button class="status-indicator">alive</button>
+        <div class="death-selection" v-if="false">
+            <button>night</button>
+            <button>1</button>
+        </div>
+        <div class="aliases">
+            <input type="text" ref="aliasInput" v-if="isAddAlias" @blur="addAlias" @keyup="handleKeyupAlias" />
+            <button @click="showAliasInput">+</button>
+        </div>
     </div>
 </template>
 
@@ -18,16 +29,41 @@ import { Player } from "@/Player";
 })
 export default class PlayerItem extends Vue {
     @Prop() private name!: string;
-    private isEditMode: boolean = false;
+    private isEditName: boolean = false;
+    private isAddAlias: boolean = false;
 
     edit(): void {
-        this.isEditMode = true;
+        this.isEditName = true;
         Vue.nextTick(() => {
             (this.$refs.edit as HTMLInputElement).focus();
         });
     }
 
-    keyUp(e: KeyboardEvent) {
+    handleKeyupAlias(e: KeyboardEvent): void {
+        if (e.keyCode === 13) {
+            this.addAlias(e);
+        }
+    }
+
+    addAlias(e: Event): void {
+        const alias = (e.target as HTMLInputElement).value;
+        if (alias.length) {
+            this.isAddAlias = false;
+            this.$store.commit("addAlias", {
+                player: this.name,
+                alias: alias,
+            });
+        }
+    }
+
+    showAliasInput(): void {
+        this.isAddAlias = !this.isAddAlias;
+        Vue.nextTick(() => {
+            (this.$refs.aliasInput as HTMLInputElement).focus();
+        });
+    }
+
+    handleKeyup(e: KeyboardEvent) {
         if (e.keyCode === 13) {
             this.stopEdit(e);
         }
@@ -40,7 +76,7 @@ export default class PlayerItem extends Vue {
                 oldName: this.$props.name,
                 newName: newValue,
             });
-            this.isEditMode = false;
+            this.isEditName = false;
         }
     }
 
@@ -51,7 +87,19 @@ export default class PlayerItem extends Vue {
 </script>
 
 <style scoped>
+.player-item {
+    display: inline-block;
+}
+
 label {
     cursor: text;
+}
+
+.death-selection {
+    display: inline-block;
+}
+
+.aliases {
+    display: inline-block;
 }
 </style>
